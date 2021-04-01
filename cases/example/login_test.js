3d4fa39a923e944ad318982dc4b5be4561cc0e41
Kostikans <48389219+Kostikans@users.noreply.github.com>
2021-04-01 21:45:19 +0300

    up

--- cases/example/login_test.js
+++ cases/example/login_test.js
@@ -1,23 +1,10 @@
-import main from '../../steps/main/main';
 import account from '../../steps/account';
 
-// пример теста
-describe('test id', () => {
+
+describe('test login', () => {
 
 	it('Авторизоваться', () => {
 		account.open('https://studhunt.ru/auth');
 		account.login();
-	});
-});
-
-describe('test id', () => {
-	
-	beforeEach(() =>{
-		account.auth()
-	})
-
-	it('Проверить переход на страницу с вакансиями по полю “профессии” при клике на поиск', () => {	
-		main.waitForPage();
-		main.searchByProfession();
 	});
 });
---
+++ cases/example/search_test.js
@@ -0,0 +1,15 @@
+import main from '../../steps/main/main';
+import account from '../../steps/account';
+
+
+describe('test search', () => {
+	
+	beforeEach(() =>{
+		account.auth()
+	})
+
+	it('Проверить переход на страницу с вакансиями по полю “профессии” при клике на поиск', () => {	
+		main.waitForPage();
+		main.searchByProfession();
+	});
+});
