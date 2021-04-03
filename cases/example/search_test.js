e45b0f1b839ba169c668b5d89e42f4336a508641
Kostikans <48389219+Kostikans@users.noreply.github.com>
2021-04-03 13:08:08 +0300

    fix

--- cases/example/search_test.js
+++ cases/example/search_test.js
@@ -3,7 +3,7 @@ import main from '../../steps/main/main';
 
 describe('test search', () => {
 	
-	it('Проверить переход на страницу с вакансиями по полю “профессии” при клике на поиск', () => {	
+	it('Поиск по професии на главной странице', () => {	
 		main.open('https://studhunt.ru')
         main.searchByProfession();
         main.checkVacTitles();
