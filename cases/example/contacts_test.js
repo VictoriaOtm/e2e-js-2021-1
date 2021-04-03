a659e99430f4645dc01f0a1e33316b7e0878e58e
nathanKith <ntolparov@gmail.com>
2021-04-04 01:26:24 +0300

    *fix after review

--- cases/example/contacts_test.js
+++ cases/example/contacts_test.js
@@ -1,8 +1,8 @@
 import account from '../../steps/account';
 import contacts from '../../steps/contacts';
 
-describe('test id.mail.ru contacts', () => {
-    beforeEach(() => {
+describe('test id.mail.ru раздел контакты', () => {
+    before(() => {
         account.open('https://account.mail.ru');
         account.login();
         account.open('https://id.mail.ru');
--- cases/example/personal_data_test.js
+++ cases/example/personal_data_test.js
@@ -1,14 +1,14 @@
 import account from '../../steps/account';
 import personalData from '../../steps/personalData';
 
-describe('test id.mail.ru personal data', () => {
-    beforeEach(() => {
+describe('test id.mail.ru раздел личные данные', () => {
+    before(() => {
         account.open('https://account.mail.ru');
         account.login();
         account.open('https://id.mail.ru');
     });
 
-    it('Переход по ссылке "Настройки личных данных".', () => {
+    it('Перейти по ссылке "Настройки личных данных" и ввести имя длиннее 40 символов.', () => {
         personalData.waitForPersonalDataLink();
         personalData.openPersonalData();
         personalData.editFirstName();
